d6c4e08be4bc190c9dab03461cffce787abaa9f5
daniel-mahmoodi <mahmoodidanial2@gmail.com>
2021-06-20 13:43:31 +0430

    start to js

---
+++ index.js
@@ -0,0 +1,8 @@
+function modifyText(new_text) {
+    const t2 = document.getElementById("");
+    t2.firstChild.nodeValue = new_text;
+}
+
+const displyNumber = document.getElementById("disply")
+
+displyNumber.addEventListener("click", function() { console.log('hello') })
